a20b7ede989026c0e37dcc29206ac701f8497e7b
Ignacio Losiggio <iglosiggio@gmail.com>
2019-03-30 18:33:57 -0300

    Agregada la infraesctructura para testear y construir el TP

---
+++ algos.h
@@ -0,0 +1,9 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+
+#include <cstdint>
+
+uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores);
+extern const char* algo;
+
+#endif
---
+++ algos/dinamica.cpp
@@ -0,0 +1,7 @@
+#include <cstdint>
+
+const char* algo = __FILE__;
+
+uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores) {
+	return 0;
+}
