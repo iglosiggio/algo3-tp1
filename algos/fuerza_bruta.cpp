6ac48afc2f8124aba028198ecc90bf30bfb65e9e
Ignacio Losiggio <iglosiggio@gmail.com>
2019-03-30 19:16:37 -0300

    Algoritmo base de fuerza bruta, n limitado a 64

--- algos/fuerza_bruta.cpp
+++ algos/fuerza_bruta.cpp
@@ -2,6 +2,41 @@
 
 const char* algo = __FILE__;
 
+#define BIT(n) (1 << n)
+
+inline uint64_t suma(uint64_t conjunto, uint32_t* valores) {
+	uint64_t resultado = 0;
+
+	for (int i = 0; i < 64; i++)
+		if (conjunto & BIT(i))
+			resultado += valores[i];
+
+	return resultado;
+}
+
+inline bool no_se_pasa(uint64_t conjunto, uint32_t limite, uint32_t* pesos) {
+	uint64_t peso = 0;
+
+	for (int i = 0; i < 64 && peso < limite; i++)
+		if (conjunto & BIT(i))
+			peso += pesos[i];
+
+	return peso < limite;
+}
+
 uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores) {
-	return 0;
+	uint64_t mejor = 0;
+	uint64_t conjunto = 1;
+	uint64_t mod = BIT(n);
+
+	do {
+		if (no_se_pasa(conjunto, w, pesos)) {
+			uint64_t valor = suma(conjunto, valores);
+			if (valor > mejor)
+				mejor = valor;
+		}
+		conjunto = (conjunto + 1) % mod;
+	} while (conjunto != 0);
+
+	return mejor;
 }
