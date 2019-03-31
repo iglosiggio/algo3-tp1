e253fec9ef09ff053194ca03eceb25692e8236b1
federico sabatini <SabatiniFedericoAgustin@gmail.com>
2019-03-31 16:29:36 -0300

    backtracking_fact: Implementación inicial

--- algos/backtracking_fact.cpp
+++ algos/backtracking_fact.cpp
@@ -1,7 +1,71 @@
 #include <cstdint>
+#include <utility>
+#include <algorithm>
+
+#include <iostream>
 
 const char* algo = __FILE__;
 
+struct problema {
+	uint32_t n;
+	uint32_t w;
+	uint32_t* pesos;
+	uint32_t* valores;
+};
+
+struct solucion {
+	uint32_t i;
+	uint64_t peso_actual;
+	uint64_t peso_restante;
+	uint64_t valor_actual;
+};
+
+uint64_t backtracking(struct problema p, struct solucion s) {
+	uint64_t nuevo_peso = s.peso_actual + p.pesos[s.i];
+	uint64_t nuevo_valor = s.valor_actual + p.valores[s.i];
+
+	if (s.peso_actual + s.peso_restante <= p.w) {
+		for(int i=s.i; i < p.n ; i++)
+			s.valor_actual += p.valores[i];
+		return s.valor_actual;
+	}
+
+	/* Podemos cortar porque ordenamos según los pesos */
+	if (nuevo_peso > p.w)
+		return s.valor_actual;
+
+	if (nuevo_peso == p.w) {
+		s.i++;
+		s.peso_restante -= p.pesos[s.i];
+		return std::max(nuevo_valor, backtracking(p, s));
+	}
+
+	if (s.i < p.n - 1) {
+		s.i++;
+		s.peso_actual = nuevo_peso;
+		s.peso_restante -= p.pesos[s.i];
+		s.valor_actual = nuevo_valor;
+
+		return backtracking(p, s);
+	}
+
+	return nuevo_valor;
+}
+
 uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores) {
-	return 0;
+	/* Ordenamos en n^2 */
+	for (int i = 0; i < n; i++)
+		for (int j = i; j < n; j++)
+			if(pesos[i] > pesos[j]) {
+				std::swap(pesos[i], pesos[j]);
+				std::swap(valores[i], valores[j]);
+			}
+
+	/* Calculamos la suma */
+	uint64_t suma = 0;
+	for (int i = 0; i < n; i++)
+		suma += pesos[i];
+	
+
+	return backtracking({n, w, pesos, valores}, {0, 0, suma, 0});
 }
