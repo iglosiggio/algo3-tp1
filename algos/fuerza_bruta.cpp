6b584775068b840dc844ae03a17730fac7f40faa
Ignacio Losiggio <iglosiggio@gmail.com>
2019-03-31 17:47:59 -0300

    fuerza_bruta: Fixear bugs en la implementación

--- algos/fuerza_bruta.cpp
+++ algos/fuerza_bruta.cpp
@@ -2,7 +2,7 @@
 
 const char* algo = __FILE__;
 
-#define BIT(n) (1 << n)
+#define BIT(n) (1LL << n)
 
 inline uint64_t suma(uint64_t conjunto, uint32_t* valores) {
 	uint64_t resultado = 0;
@@ -17,11 +17,11 @@ inline uint64_t suma(uint64_t conjunto, uint32_t* valores) {
 inline bool no_se_pasa(uint64_t conjunto, uint32_t limite, uint32_t* pesos) {
 	uint64_t peso = 0;
 
-	for (int i = 0; i < 64 && peso < limite; i++)
+	for (int i = 0; i < 64 && peso <= limite; i++)
 		if (conjunto & BIT(i))
 			peso += pesos[i];
 
-	return peso < limite;
+	return peso <= limite;
 }
 
 uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores) {
@@ -29,14 +29,14 @@ uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores) {
 	uint64_t conjunto = 1;
 	uint64_t mod = BIT(n);
 
-	do {
+	while (conjunto < mod) {
 		if (no_se_pasa(conjunto, w, pesos)) {
 			uint64_t valor = suma(conjunto, valores);
 			if (valor > mejor)
 				mejor = valor;
 		}
 		conjunto++;
-	} while (conjunto < mod);
+	}
 
 	return mejor;
 }
