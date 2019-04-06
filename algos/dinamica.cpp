5951c3dbd79fb9299cdaf249ecd9acc5092bbe5b
federico sabatini <SabatiniFedericoAgustin@gmail.com>
2019-04-06 14:02:06 -0300

    first topdown dynamic implementation =D

--- algos/dinamica.cpp
+++ algos/dinamica.cpp
@@ -9,22 +9,29 @@ const char* algo = __FILE__;
 
 using Matriz = std::vector<std::vector<int32_t> >;
 
-uint64_t dynamic_value_maximizer(uint32_t i, uint32_t w, uint32_t* p, uint32_t* v, std::vector<std::vector<int32_t> > memoria){
+Matriz memoria;
+std::vector<int32_t> lista;
 
-	if(memoria[i][w] != -1) 
-		return memoria[i][w];
-
-	uint32_t result = 0;
+uint32_t dynamic_value_maximizer(uint32_t i, uint32_t w, uint32_t* p, uint32_t* v, std::vector<std::vector<int32_t> > memoria){
 
 	if( i == 0 || w <= 0){
 		return 0;
 	}
 
-	if (p[i] > w) {
+	if(memoria[i-1][w] != -1) {
+		return memoria[i-1][w];
+	}
+
+	uint32_t result = 0;
+
+
+	if (p[i-1] > w) {
 		result = dynamic_value_maximizer(i-1, w, p, v, memoria);
 	} else {
-		result = std::max(dynamic_value_maximizer(i-1, w, p, v, memoria), v[i] + dynamic_value_maximizer(i-1, w - p[i], p, v, memoria));
+		result = std::max(dynamic_value_maximizer(i-1, w, p, v, memoria), v[i-1] + dynamic_value_maximizer(i-1, w - p[i-1], p, v, memoria));
 	}
+
+
 
 	return result;
 }
@@ -33,14 +40,10 @@ uint64_t dynamic_value_maximizer(uint32_t i, uint32_t w, uint32_t* p, uint32_t* 
 uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores) {
 	/* defino una matriz de memorizacion  */
 
-	Matriz memoria;
-	std::vector<int32_t> lista;
-
 	lista.resize(w, -1);
 	memoria.resize(n, lista);
 
-	uint64_t result =  dynamic_value_maximizer(n-1,w-1,pesos,valores, memoria);
-	
+	uint32_t result =  dynamic_value_maximizer(n,w-1,pesos,valores, memoria);
 
 	return result;
 
