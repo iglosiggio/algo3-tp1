b1553e32d9dbea2471bf7267538cf3c992ea0c4e
federico sabatini <SabatiniFedericoAgustin@gmail.com>
2019-04-06 15:10:22 -0300

    ok era un forro, ya esta

--- algos/dinamica.cpp
+++ algos/dinamica.cpp
@@ -12,14 +12,13 @@ using Matriz = std::vector<std::vector<int32_t> >;
 Matriz memoria;
 std::vector<int32_t> lista;
 
-uint32_t dynamic_value_maximizer(uint32_t i, uint32_t peso_restante, uint32_t* p, uint32_t* v, std::vector<std::vector<int32_t> > memoria){
+uint32_t dynamic_value_maximizer(uint32_t i, uint32_t peso_restante, uint32_t* p, uint32_t* v){
 
 	if( i == 0 || peso_restante <= 0){
 		return 0;
 	}
 
 	if(memoria[i-1][peso_restante] != -1) {
-		std::cout << memoria[i-1][peso_restante] << std::endl;
 		return memoria[i-1][peso_restante];
 	}
 
@@ -27,10 +26,10 @@ uint32_t dynamic_value_maximizer(uint32_t i, uint32_t peso_restante, uint32_t* p
 
 
 	if (p[i-1] > peso_restante) {
-		result = dynamic_value_maximizer(i-1, peso_restante, p, v, memoria);
+		result = dynamic_value_maximizer(i-1, peso_restante, p, v);
 	} else {
-		result = std::max(dynamic_value_maximizer(i-1, peso_restante, p, v, memoria), 
-					v[i-1] + dynamic_value_maximizer(i-1, peso_restante - p[i-1], p, v, memoria));
+		result = std::max(dynamic_value_maximizer(i-1, peso_restante, p, v), 
+					v[i-1] + dynamic_value_maximizer(i-1, peso_restante - p[i-1], p, v));
 	}
 	memoria[i-1][peso_restante] = result;
 
@@ -44,7 +43,7 @@ uint64_t mochila(uint32_t n, uint32_t w, uint32_t* pesos, uint32_t* valores) {
 	lista.resize(w, -1);
 	memoria.resize(n, lista);
 
-	uint32_t result =  dynamic_value_maximizer(n,w-1,pesos,valores, memoria);
+	uint32_t result =  dynamic_value_maximizer(n,w-1,pesos,valores);
 
 	return result;
 
